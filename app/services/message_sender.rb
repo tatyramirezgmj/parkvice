21b1e1efc21f9d2231853678cecafca1062bddd3
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-07-02 20:10:50 -0400

    Text messages working

--- app/services/message_sender.rb
+++ app/services/message_sender.rb
@@ -10,7 +10,7 @@ class MessageSender
 
   def send_messages
     @phone_numbers.each do |ph|
-      @client.messages.create(:from => twilio_number, :to => ph , :body => "Pay your parking!")
+      @client.messages.create(:from => twilio_number, :to => ph , :body => "This is Parkvice avoiding you a ticket! Parking ticket officer near Wyncode, you can pay here => https://goo.gl/2RLLZc")
     end
   end
 
@@ -19,6 +19,6 @@ class MessageSender
   private
 
   def twilio_number
-    twilio_number = '+19543291001'
+    twilio_number = '+17864602862'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  get 'notifications/new' 
+  get 'notifications/new'
   post 'notifications/create' => 'notifications#create', as: "sendsms"
 
 
